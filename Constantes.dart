1ce528452ee002e66654e656730b806e177ddb3d
Sebastián <sgarcia710@outlook.com>
2020-01-26 19:00:14 -0400

    Clase 8

---
+++ Constantes.dart
@@ -0,0 +1,10 @@
+// Final y Const
+main(List<String> args) {
+  // Final: Únicamente será alojada en memoria al momento de usarla. Es decir, al momento en el que realmente el código haga uso de ella.
+  final String nombre = "Sebastián";
+  print(nombre);
+
+  // Const: Es declara y alojada en memoria al momento de ponerla en nuestro código.
+  const edad = 23;
+  print(edad);
+}
