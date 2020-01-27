396ea394e938474571eb336ae029081e4f108bcd
Sebastián <sgarcia710@outlook.com>
2020-01-26 20:35:46 -0400

    Clase 15

---
+++ WhileDoWhile.dart
@@ -0,0 +1,14 @@
+// Dart y Do While
+main(List<String> args) {
+  // While
+  int counter = 10;
+  while (counter > 0) {
+    print("Actual value: $counter");
+    counter--;
+  }
+  counter = 15;
+  do {
+    print("Actual value is: $counter");
+    counter--;
+  } while (counter > 10);
+}
